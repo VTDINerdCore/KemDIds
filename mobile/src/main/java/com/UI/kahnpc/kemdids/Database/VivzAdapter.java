f4c68f1a5c8dab8704e18ffaffd3553b8d9fdf72
VTDINerdCore <kemdids@gmail.com>
2014-08-03 21:59:45 -0400

    Created control panel activity

---
+++ mobile/src/main/java/com/UI/kahnpc/kemdids/Database/VivzAdapter.java
@@ -0,0 +1,7 @@
+package com.UI.kahnpc.kemdids.Database;
+
+/**
+ * Created by kahnpc on 7/27/14.
+ */
+public class VivzAdapter {
+}
---
+++ mobile/src/main/java/com/UI/kahnpc/kemdids/Helpers/Message.java
@@ -0,0 +1,7 @@
+package com.UI.kahnpc.kemdids.Helpers;
+
+/**
+ * Created by kahnpc on 7/28/14.
+ */
+public class Message {
+}
